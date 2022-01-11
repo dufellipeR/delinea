bd64046cab996bc3c0ca045a4afcee30f81a2d66
dufellipeR <eduardo.fellipe30@gmail.com>
2022-01-10 22:57:40 -0300

    google social auth system

---
+++ social_auth/serializers.py
@@ -0,0 +1,33 @@
+from logging import log
+from rest_framework import serializers
+from . import google
+from .register import register_social_user
+import os
+from rest_framework.exceptions import AuthenticationFailed
+
+class GoogleSocialAuthSerializer(serializers.Serializer):
+    auth_token = serializers.CharField()
+
+    def validate_auth_token(self, auth_token):
+        user_data = google.Google.validate(auth_token)
+        try:
+            user_data['sub']
+
+        except:
+            raise serializers.ValidationError(
+                'The token is invalid or expired. Please login again.'
+            )
+
+        # Code case you want to receive from an exclusive frontend-app
+        # if user_data['aud'] != os.environ.get('GOOGLE_CLIENT_ID'):
+
+        #     raise AuthenticationFailed('Not known client')
+
+        user_id = user_data['sub']
+        email = user_data['email']
+        name = user_data['name']
+        provider = 'google'
+
+        return register_social_user(
+            provider=provider, user_id=user_id, email=email, name=name)
+
